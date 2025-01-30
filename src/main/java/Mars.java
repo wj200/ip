1f072f11d1db292a43027a61edaf82bea174817a
wj <boo.wenjun2002@gmail.com>
2025-01-30 14:23:36 +0800

    Rename,Greet,Exit

---
+++ src/main/java/Mars.java
@@ -0,0 +1,19 @@
+public class Mars {
+    public static void main(String[] args) {
+        /*
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+         */
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm Mars\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________");
+
+    }
+}
