120bd07e5a00f2ce3208e61ab8d8d16c7306a1f1
wj <boo.wenjun2002@gmail.com>
2025-01-30 20:49:18 +0800

    Level 2.Add, List

--- src/main/java/Mars.java
+++ src/main/java/Mars.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Mars {
     public static void main(String[] args) {
@@ -20,6 +21,7 @@ public class Mars {
                 "____________________________________________________________"); */
         Mars mars = new Mars();
         mars.echo();
+        mars.store();
     }
 
     /*Level 1. Echo*/
@@ -38,4 +40,35 @@ public class Mars {
         reader.close();
     }
 
+    /*Level 2. Add, List */
+    private void store(){
+        List<String> arrayList = new ArrayList<String>();
+        System.out.println(" Hello! I'm Mars\n What can I do for you?");
+        Scanner reader = new Scanner(System.in);
+        String n = reader.nextLine();
+        while(!n.equals("bye")){
+            arrayList.add(n);
+            if(n.equals("list")){
+                Mars.list(arrayList);
+            }
+            String output = "____________________________________________________________\n" +
+                    "added: " + n +
+                    "\n____________________________________________________________\n";
+            System.out.println(output);
+            n = reader.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        reader.close();
+    }
+
+    private static void list(List<String> lst){
+        String[] strArray = lst.toArray();
+        System.out.println("____________________________________________________________\n");
+        int i = 0;
+        while(i < strArray.length){
+            System.out.println(i + "." + strArray[i]);
+            i += 1;
+        }
+        System.out.println("____________________________________________________________\n");
+    }
 }
