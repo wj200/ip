61a304a00a07241b3431e6b26f952bdbd685012f
wj <boo.wenjun2002@gmail.com>
2025-01-30 15:01:25 +0800

    Level 1. Improve the skeletal version of Duke

--- src/main/java/Mars.java
+++ src/main/java/Mars.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Mars {
     public static void main(String[] args) {
-        /*
+        /* PRE- COMMITS
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -8,12 +10,31 @@ public class Mars {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
          */
+
+        /* LEVEL 0. Rename, Greet, Exit
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm Mars\n" +
                 " What can I do for you?\n" +
                 "____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________");
-
+                "____________________________________________________________"); */
+        Mars mars = new Mars();
+        mars.echo();
     }
+
+    private void echo(){
+        System.out.println(" Hello! I'm Mars\n What can I do for you?");
+        Scanner reader = new Scanner(System.in);
+        String n = reader.nextLine();
+
+        while(!n.equals("bye")){
+            System.out.println("____________________________________________________________\n" +
+                                n +
+                                "\n____________________________________________________________\n");
+            n = reader.nextLine();
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        reader.close();
+    }
+
 }
